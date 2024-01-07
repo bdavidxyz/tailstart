43d4465b55f13a9411d1f0beb5112985f76cad18
David Boureau <bdavidxyz@gmail.com>
2024-01-07 21:53:08 +0100

    feat: working navbar auth, registration layout ok

---
+++ files/app/controllers/home_controller.rb
@@ -0,0 +1,17 @@
+class HomeController < ApplicationController
+
+  skip_before_action :authenticate
+  before_action :soft_authenticate
+
+  def index
+  end
+  
+  private
+
+  def soft_authenticate
+    if session_record = Session.find_by_id(cookies.signed[:session_token])
+      Current.session = session_record
+    end
+  end
+
+end
